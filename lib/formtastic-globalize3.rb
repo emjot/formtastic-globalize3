8e76cea58a4b70f3f41af7774c858c9132a5fec9
mherold <herold@emjot.de>
2014-08-14 12:17:10 +0200

    Bump globalize3 dependency and remove patch

--- lib/formtastic-globalize3.rb
+++ lib/formtastic-globalize3.rb
@@ -1,7 +1,4 @@
 require "formtastic/globalize3/version"
-require "formtastic/globalize3/helpers/input_helper"
 
 require 'globalize'
 require 'formtastic'
-
-Formtastic::FormBuilder.send(:include, Formtastic::Globalize3::Helpers::InputHelper)
